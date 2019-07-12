6ee96a86eaeb473636642f45e85b59f3d67a897e
Circle Yu <circleyu@GCS-Circle.local>
2019-07-12 11:09:37 +0800

    添加LoginService的etcd的註冊行為，以及添加vendor資料夾內容

--- LoginService/main.go
+++ LoginService/main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-	"./protocol"
-
-	"golang.org/x/net/context"
+	protocol "github.com/CircleYu/GameServer/LoginService/protocol"
+	grpclb "github.com/CircleYu/GameServer/etcdv3"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 )
 
-// server 建構體會實作 Calculator 的 gRPC 伺服器。
 type server struct{}
 
 func (s *server) SignIn(ctx context.Context, in *protocol.SignInRequest) (*protocol.SignInResponse, error) {
@@ -26,24 +29,38 @@ func (s *server) SignOut(ctx context.Context, in *protocol.SignOutRequest) (*pro
 	return &protocol.SignOutResponse{}, nil
 }
 
+var (
+	serv = flag.String("service", "login_service", "service name")
+	host = flag.String("host", "localhost", "listening host")
+	port = flag.String("port", "50001", "listening port")
+	reg  = flag.String("reg", "http://localhost:2379", "register etcd address")
+)
+
 func main() {
-	// 監聽指定埠口，這樣服務才能在該埠口執行。
-	lis, err := net.Listen("tcp", ":50051")
+
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", net.JoinHostPort(*host, *port))
 	if err != nil {
-		log.Fatalf("無法監聽該埠口：%v", err)
+		panic(err)
 	}
 
-	// 建立新 gRPC 伺服器並註冊 Calculator 服務。
+	err = grpclb.Register(*reg, *serv, *host, *port, time.Second*10, 15)
+	if err != nil {
+		panic(err)
+	}
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL, syscall.SIGHUP, syscall.SIGQUIT)
+	go func() {
+		s := <-ch
+		log.Printf("receive signal '%v'", s)
+		grpclb.UnRegister()
+		os.Exit(1)
+	}()
+
+	log.Printf("starting login service at %s", *port)
 	s := grpc.NewServer()
 	protocol.RegisterLoginControllerServer(s, &server{})
-
-	// 在 gRPC 伺服器上註冊反射服務。
-	reflection.Register(s)
-
-	log.Print("Service Start !!!")
-
-	// 開始在指定埠口中服務。
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("無法提供服務：%v", err)
-	}
+	s.Serve(lis)
 }
